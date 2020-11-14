a8dbe162a596870266df7f0bdef7b2685ab8c2e6
Roberto Corti <robi290596@gmail.com>
2020-11-14 10:25:32 +0100

    added some comments

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,12 @@
 package dssc.assignment.fizzbuzz;
+/*
+Write a Java program that prints the numbers from 1 to 100 to the "standard" output stream and follows these rules.
 
+    For multiples of 3 it prints Fizz instead of the number.
+    For multiples of 5 it prints Buzz instead of the number.
+    For numbers which are multiples of both 3 and 5 it prints FizzBuzz.
+
+*/
 public class FizzBuzz {
 
     public static void main(String... args) {
