3303d7aa72482d406f79d36a1a7ddab81469a77d
Roberto <robi290596@gmail.com>
2020-11-21 11:59:20 +0100

    modified test

--- src/test/java/dssc/assignment/fizzbuzz/Convert.java
+++ src/test/java/dssc/assignment/fizzbuzz/Convert.java
@@ -12,31 +12,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Convert {
 
+    private FizzBuzz fizzBuzz = new FizzBuzz();
+
     @ParameterizedTest
     @CsvSource({"1, 1", "2, 2", "41, 41"})
     void numbers_not_divisible_by_3_or_5_to_strings(int number, String expected){
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals(expected, fizzBuzz.convert(number));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {3, 6, 24})
     void multiples_of_3_to_Fizz(int number){
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("Fizz", fizzBuzz.convert(number));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {5, 10, 100})
     void multiples_of_5_to_Buzz(int number){
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("Buzz", fizzBuzz.convert(number));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {15, 45, 75})
     void multiples_of_3_and_5_to_FizzBuzz(int number){
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("FizzBuzz", fizzBuzz.convert(number));
     }
 }
