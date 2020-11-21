51ae9f3b5734405ea794a3a409530701d7e9f7c8
Roberto <robi290596@gmail.com>
2020-11-21 10:45:35 +0100

    converted 40 to string(40)

--- src/test/java/dssc/assignment/fizzbuzz/ConvertNumbersToStrings.java
+++ src/test/java/dssc/assignment/fizzbuzz/ConvertNumbersToStrings.java
@@ -12,9 +12,16 @@ public class ConvertNumbersToStrings {
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("1", fizzBuzz.convert(1));
     }
+
     @Test
     void number_2(){
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("2", fizzBuzz.convert(2));
+    }
+
+    @Test
+    void number_40(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("40", fizzBuzz.convert(40));
     }
 }
